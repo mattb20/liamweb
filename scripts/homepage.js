d8bde08d89c3067368ca7c9199a56de79249202a
mattb20 <matt_burgoyne@live.co.uk>
2018-10-28 12:28:45 +0000

    add fab icons with links to media on homepage

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,7 @@
 var home = new Vue({
   el: '#home',
   data: {
-    welcome: 'Welcome to the website of Liam Burgoyne'
+    welcome: 'Welcome to the website of Liam Burgoyne',
+    mediaLinks: 'Please find links to my active media below:'
   }
 })
